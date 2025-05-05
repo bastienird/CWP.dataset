00425c61076750357304b448e0524a35029e9066
g2oi <bastien.grasset@ird.fr>
2025-05-05 13:38:48 +0400

    hotfix: testing

--- R/geographic_diff.R
+++ R/geographic_diff.R
@@ -17,14 +17,14 @@
 #' @importFrom tmap tm_shape tm_fill tm_facets tm_layout tm_borders
 #' @importFrom ggplot2 ggsave
 #' @importFrom sf st_as_sf
-geographic_diff <- function(init, final, shapefile_fix, parameter_geographical_dimension, 
-                            parameter_geographical_dimension_groupping, continent, plotting_type, 
+geographic_diff <- function(init, final, shapefile_fix, parameter_geographical_dimension,
+                            parameter_geographical_dimension_groupping, continent, plotting_type,
                             titre_1, titre_2, outputonly) {
-  
-  geographic_dimension <- fonction_groupement(c(parameter_geographical_dimension, parameter_geographical_dimension_groupping), 
-                                              init = init , final = final) %>% 
+
+  geographic_dimension <- fonction_groupement(c(parameter_geographical_dimension, parameter_geographical_dimension_groupping),
+                                              init = init , final = final) %>%
     dplyr::filter(value_sum_1 != 0 | value_sum_2 != 0)
-  
+
   breaks <- dplyr::inner_join(shapefile_fix %>% dplyr::select(cwp_code, geom), geographic_dimension, by = c("cwp_code"="Precision")) %>%
     dplyr::mutate(`Impact on the data` = dplyr::case_when(`Difference (in %)` == Inf ~ "Appearing data",
                                                           Inf > `Difference (in %)`  & `Difference (in %)` >= 100 ~ "Gain (more than double)",
@@ -32,24 +32,44 @@ geographic_diff <- function(init, final, shapefile_fix, parameter_geographical_d
                                                           `Difference (in %)` == 0 ~ "No differences",
                                                           0 > `Difference (in %)`  & -100 < `Difference (in %)` ~ "Loss",
                                                           `Difference (in %)` == -100 ~  "All data lost"))
-  
-  breaks <- breaks %>% dplyr::ungroup()
-  
-  breaks$`Impact on the data` <- factor(breaks$`Impact on the data`, 
-                                        levels = c("Appearing data", "Gain (more than double)", "Gain", 
-                                                   "No differences", "Loss", "All data lost"))
-  
-  image <- tmap::tm_shape(breaks) +
-    tmap::tm_fill("Impact on the data", palette = "-PiYG", id = "name") +
-    tmap::tm_facets(by = c("measurement_unit", parameter_geographical_dimension_groupping), free.scales = FALSE, free.coords = FALSE) + # do not put free coords to true it creates bug
-    tmap::tm_layout(legend.outside = TRUE)
 
-  # if (plotting_type == "plot") {
-    image <- image+tmap::tm_shape(continent) + tmap::tm_borders() 
-    # image_to_save <- image
-  # } else {
-  #   # image_to_save <- tmap::tm_shape(continent) + tmap::tm_borders() + image
-  # }
+  breaks <- breaks %>% dplyr::ungroup()
+
+  breaks$`Impact on the data` <- factor(breaks$`Impact on the data`,
+                                        levels = c("Appearing data", "Gain (more than double)", "Gain",
+                                                   "No differences", "Loss", "All data lost"))
+  browser()
+  if (plotting_type == "view") {
+    image <- tmap::tm_shape(breaks) +
+      tmap::tm_fill(
+        col = "Impact on the data",
+        palette = "-PiYG",
+        popup.vars = c(
+          "Unité" = "measurement_unit",
+          "Impact" = "Impact on the data"
+        )
+      ) +
+      tmap::tm_facets(
+        by = c("measurement_unit", parameter_geographical_dimension_groupping),
+        free.scales = FALSE,
+        free.coords = FALSE
+      ) +
+      tmap::tm_layout(legend.outside = TRUE)
+  } else {
+    image <- tmap::tm_shape(breaks) +
+      tmap::tm_fill(
+        col = "Impact on the data",
+        palette = "-PiYG"
+      ) +
+      tmap::tm_facets(
+        by = c("measurement_unit", parameter_geographical_dimension_groupping),
+        free.scales = FALSE,
+        free.coords = FALSE
+      ) +
+      tmap::tm_layout(legend.outside = TRUE)
+    image <- image+tmap::tm_shape(continent) + tmap::tm_borders()
+  }
+
   title = paste0("Spatial differences between ", titre_1, " and ", titre_2, " dataset")
   breaks$geom <- NULL
   return(list(title = title, plott = image, tableforimage = breaks))
