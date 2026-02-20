3f8aff3957d9f4ed28888f56f8d10e9f0910210e
BastienGrassetIRD <bastien.grasset@ird.fr>
2026-02-20 13:04:39 +0400

    feature: indicate better the identical years

--- R/compare_temporal_differences.R
+++ R/compare_temporal_differences.R
@@ -31,16 +31,39 @@ compare_temporal_differences <- function(parameter_time_dimension, init, final, 
   }
 
   timediffplot <- lapply(parameter_time_dimension, function(filtering_unit, dataframe) {
-    ggplot(dataframe %>% dplyr::filter(Dimension == filtering_unit) %>% dplyr::mutate(Time = as.Date(Precision))) +
+    df_plot <- dataframe %>%
+      dplyr::filter(Dimension == filtering_unit) %>%
+      dplyr::mutate(Time = as.Date(Precision))
+
+    df_equal <- df_plot %>%
+      dplyr::filter(!is.na(`Difference (in %)`), `Difference (in %)` == 0)
+
+    ggplot(df_plot) +
       aes(x = Time, y = `Difference (in %)`) +
-      geom_line(size = 0.5) +
-      scale_fill_hue(direction = 1) +
+      # Option 3: transparency + points
+      geom_line(linewidth = 0.5, alpha = 0.7) +
+      geom_point(size = 1.8, alpha = 0.7) +
+      # Option 4: explicit marker where the two datasets are identical
+      geom_point(
+        data = df_equal,
+        inherit.aes = TRUE,
+        shape = 21, fill = "white", color = "black",
+        size = 2.8, stroke = 0.8
+      ) +
       theme(legend.position = "top") +
-      facet_wrap(vars(measurement_unit), nrow = 2L) + theme_bw() +
-      labs(x = filtering_unit) +
+      theme_bw() +
+      labs(
+        x = filtering_unit,
+        caption = ifelse(
+          nrow(df_equal) != 0,
+          "White markers indicate identical values between the two datasets (difference = 0).",
+          ""
+        )
+      ) +
       facet_grid(rows = vars(measurement_unit), scales = "free_y")
 
   }, dataframe = Groupped_all_time)
+
 
   titles <- paste0("Difference in percent of value for the dimension ", parameter_time_dimension, " for ", titre_1, " and ", titre_2, " dataset ")
 
